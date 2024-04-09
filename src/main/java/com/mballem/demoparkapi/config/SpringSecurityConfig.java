b63ad548ceef4bd902f5ea329e2b8e3e6b852028
Guterresthomas <guterresmikael@gmail.com>
2024-04-09 14:17:41 -0300

    CLass springSecurity

---
+++ src/main/java/com/mballem/demoparkapi/config/SpringSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.mballem.demoparkapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableMethodSecurity
+@EnableWebMvc
+@Configuration
+public class SpringSecurityConfig {
+}
